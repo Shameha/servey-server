82d617f9c246792934331544e8f96dbcc2876300
shamehaera9 <shamehaera9@gmail.com>
2024-06-08 21:27:26 +0600

    added page

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t86vw4m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,6 +33,15 @@ async function run() {
         res.send(result)
     })
 
+app.get('/servey/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query ={_id: new ObjectId(id)};
+  const options = {
+    projection: { title: 1, description: 1 },
+  };
+  const result = await surveyCollection.findOne(query,options);
+  res.send(result)
+})
 
 
     // Send a ping to confirm a successful connection
