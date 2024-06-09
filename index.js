1c8f26812d2b18c217445d8178888f09e372f99c
shamehaera9 <shamehaera9@gmail.com>
2024-06-10 03:11:02 +0600

    adding jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -32,6 +33,22 @@ async function run() {
  
     // const Collection = client.db("surveyDb").collection("reviews");
 
+//jwt related api
+app.post('/jwt', async(req,res)=>{
+  const user = req.body;
+  const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+    expiresIn:'2h'
+  })
+  res.send({token});
+})
+
+
+app.get('/users',async(req,res)=>{
+  console.log(req.headers);
+  const result = await usersCollection.find().toArray();
+  res.send(result);
+});
+
     app.post('/users',async(req,res)=>{
   const user = req.body;
   //insert email doesnot exist
@@ -44,8 +61,42 @@ async function run() {
   res.send(result);
 })
 
+app.patch('/users/admin/:id',async(req,res)=>{
+  const id = req.params.id;
+  const filter ={_id: new ObjectId(id)};
+  const updateDoc ={
+    $set:{
+      role:'admin',
+      role:'survey'
+            
+
+    }
+  }
+  const result = await usersCollection.updateOne(filter,updateDoc)
+  res.send(result);
+})
+// app.patch('/users/survey/:id',async(req,res)=>{
+//   const id = req.params.id;
+//   const filter ={_id: new ObjectId(id)};
+//   const updateDoc ={
+//     $set:{
+//       role:'survey'
+//     }
+//   }
+//   const result = await usersCollection.updateOne(filter,updateDoc)
+//   res.send(result);
+// })
 
 
+
+app.delete('/users/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query = {_id:new ObjectId(id)}
+  const result = await usersCollection.deleteOne(query);
+  res.send(result);
+})
+
+//survay related api
     app.get("/servey",async(req,res)=>{
         const result= await surveyCollection.find().toArray();
         res.send(result)
